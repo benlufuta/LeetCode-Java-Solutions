2def24e713da0201ee498a7d73f1d0fb5ffd8e48
Ben Lufuta <ben.lufuta@gmail.com>
2026-04-04 10:11:26 -0400

    1. Two Sum

---
+++ Easy/twosum/TwoSum.java
@@ -0,0 +1,25 @@
+package Easy.twosum;
+
+class TwoSum {
+
+    //This is a brute force Solution. 
+    // Implementation of optimal solution in progress.
+    
+        public int[] twoSum(int[] nums, int target) {
+
+         for (int i = 0; i < nums.length; i++){
+    
+            for (int j = i + 1; j < nums.length; j++) {
+
+                if ((nums[i] + nums[j]) == target) {
+
+                 return new int [] {i, j};
+
+                }
+            }
+         }
+         
+         return new int [] {};
+        
+    }
+}
