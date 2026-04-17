a919bc5cabb6dcc7a8238ad762d011a1f6ceb5da
Ben Lufuta <ben.lufuta@gmail.com>
2026-04-17 10:02:57 -0400

    219. Contains Duplicate II: Efficient Solution

--- Easy/219. Contains Duplicate II/Solution.java
+++ Easy/219. Contains Duplicate II/Solution.java
@@ -4,7 +4,7 @@ class Solution {
         
         //Brute Force Solution:
         //Solution that works but not time or space complexity efficient.
-        for (int i = 0; i < nums.length; i++){
+        /*for (int i = 0; i < nums.length; i++){
 
             for (int j = i + 1; j < nums.length; j++){
 
@@ -15,7 +15,29 @@ class Solution {
             }
         }
 
-        return false;
+        return false;*/
+
+        //Time and Space complexity efficient Solution
+        //Time O(N)
+        //Space O(N)
+        HashMap <Integer, Integer> numAndIndex = new HashMap<>();
+
+            //Go through the whole array once.
+            for (int i = 0; i < nums.length; i++){
+
+                // If the current element is already in the HashMap, we’ve seen a duplicate.
+                // If the value between the current index and the stored index is
+                // less than or equal to k, return true.
+                if (numAndIndex.containsKey(nums[i]) && Math.abs(numAndIndex.get(nums[i])- i) <= k) {
+
+                        return true;
+                    }
+
+                    //Otherwise, add element to HashMap and also store its index.
+                    numAndIndex.put(nums[i], i);
+                }
+            //Return false only if no duplicate. 
+            return false;
     }
 
 }
