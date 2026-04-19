06f2153d97701ebb6dfb0a2ffd662c839daee2c3
Ben Lufuta <ben.lufuta@gmail.com>
2026-04-19 13:23:45 -0400

    125. Valid Palindrome

--- Easy/125. Valid Palindrome/Solution.java
+++ Easy/125. Valid Palindrome/Solution.java
@@ -1,26 +1,43 @@
 class Solution {
     public boolean isPalindrome(String s) {
 
-        //Working solution (somewhat efficient) but more efficient solution coming soon...
-        //Time complexity efficient but not as memory efficient.
+        /*
+         Efficient in-place two-pointer version
+         Time: O(n)
+         Space: O(1)
+        */
 
-        //Remove any non alphanumerical characters and make lowercase.
-        String cleaned = s.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
-
-        //Start and End pointers.
+        // Left starts at the beginning of the string, right starts at the end.
         int left = 0;
-        int right = cleaned.length() - 1;
+        int right = s.length() - 1;
 
         while (left < right){
 
-            //If character at left does not equal character at right return false.
-            if (cleaned.charAt(left) != cleaned.charAt(right)) {
+            // If the current left character is not alphanumeric,
+            // skip it by moving the left pointer forward.
+            if (!Character.isLetterOrDigit(s.charAt(left))){
+                left++;
+            } 
+            // If the current right character is not alphanumeric,
+            // skip it by moving the right pointer backward.
+            else if (!Character.isLetterOrDigit(s.charAt(right))){
+                right--;
+            } 
+            // Otherwise, both characters are alphanumeric:
+            // compare them in a case-insensitive way.
+            // If they differ, return false. If they match, move both pointers.
+            else {
+                
+                if (Character.toLowerCase(s.charAt(left)) != Character.toLowerCase(s.charAt(right))) { 
+                    return false;
 
-                return false;
+                } else {
+                    left++;
+                    right--;
+                }
             }
-            left++;
-            right--;
         }
-        return true;
+        // If we exit the loop without finding a mismatch, it is a palindrome.
+        return true;        
     }
 }
