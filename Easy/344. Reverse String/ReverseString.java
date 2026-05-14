cd411ac6d390accc4fe2ae3eadfcd4dd269bbb25
Ben Lufuta <ben.lufuta@gmail.com>
2026-05-13 23:42:53 -0400

    Reverse String

---
+++ Easy/344. Reverse String/ReverseString.java
@@ -0,0 +1,19 @@
+public class ReverseString {
+
+    public void reverseString(char[] s) {
+
+        int p1 = 0;
+        int p2 = s.length - 1;
+
+        while (p1 != s.length / 2) {
+
+            char temp = s[p1];
+            s[p1] = s[p2];
+            s[p2] = temp;
+
+            p1++;
+            p2--;
+        }
+        
+    }
+}
