8412884e7956c27d093084cf259ba8378f61d76d
Ben Lufuta <ben.lufuta@gmail.com>
2026-05-19 12:18:33 -0400

    977. Squares of a Sorted Array

--- Easy/977. Squares of a Sorted Array/SquaresOfASortedArray.java
+++ Easy/977. Squares of a Sorted Array/SquaresOfASortedArray.java
@@ -37,4 +37,59 @@ public class SquaresOfASortedArray {
 
         return nums;
     }
+
+    /*
+        Speed Efficent Solution
+        Time Complexity: O(N)
+        Space Complexity: O(N)
+        Idea: Utilizing two pointers from both ends to fill the result array in reverse order based on squared values.
+    
+    */
+    public int[] sortedSquares(int[] nums) {
+
+        //Serve a index tracker for new array.
+        int k = nums.length;
+        
+        //First pointer starts at beginning of the array.
+        int p1 = 0;
+        //Second pointer starts at end of the array.
+        int p2 = k - 1;
+        
+        //Will store all squared values.
+        int [] sortedNums = new int [k];
+        
+        //Help keep track current values.
+        int squaredOne = 0;
+        int squaredTwo = 0;
+      
+        while ( p1 <= p2){
+            
+            squaredOne = nums[p1] * nums[p1];
+            squaredTwo = nums[p2] * nums[p2];
+            
+            // Squared value at the second pointer is greater than
+            // or equal to the squared value at the first pointer,
+            // so store it in the next available inward index
+            // of the new array.
+            if (squaredOne <= squaredTwo){
+            
+                sortedNums[k - 1] = squaredTwo;
+                k--;
+                p2--;
+            
+            } 
+            //If above contion fails, then store value at first pointer.
+            else {
+
+                sortedNums[k - 1] = squaredOne;
+                k--;
+                p1++;
+            
+            }
+            
+        }
+      
+      return sortedNums;
+        
+    }
 }
