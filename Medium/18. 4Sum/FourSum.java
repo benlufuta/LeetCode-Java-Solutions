133dce7703eea756c48465b4d789b6d560f481a2
Ben Lufuta <ben.lufuta@gmail.com>
2026-06-18 09:32:17 -0400

    18. 4Sum

---
+++ Medium/18. 4Sum/FourSum.java
@@ -0,0 +1,63 @@
+import java.util.List;
+
+public class FourSum {
+
+    public List<List<Integer>> fourSum(int[] nums, int target) {
+
+        Arrays.sort(nums);
+        
+        List<List<Integer>> results = new ArrayList<>();
+
+        for (int i = 0; i < nums.length - 1; i++){
+
+            if (i > 0 && nums[i] == nums[i - 1]){
+
+                continue;
+            }
+
+            for (int j = i + 1; j < nums.length - 1; j++){
+
+                int left = j + 1;
+                int right = nums.length - 1;
+
+                if ( j > i + 1 && nums[j] == nums[j - 1]){
+
+                    continue;
+                }
+
+                while (left < right){
+
+                    long total = (long)nums[i] + nums[j] + nums[left] + nums[right];
+
+                    if (total == target){
+
+                      results.add(Arrays.asList(nums[i], nums[j], nums[left], nums[right]));
+
+                    left++;
+                    right--;
+
+                    while ((left >= 1 && left < right) && (nums[left] == nums[left-1])){
+
+                        left++;
+                    } 
+
+                    while ((right < nums.length - 1 && right > left) && (nums[right] == nums[right + 1])){
+
+                        right--;
+                    }  
+                    } else if (total < target){
+
+                        left++;
+                    } else {
+
+                        right--;
+                    }
+                }
+            }
+        }
+
+        return results;
+        
+    }
+    
+}
